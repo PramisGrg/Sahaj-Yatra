4f35e8917597242a70c9581030a80661d141667b
PramisGrg <gurungpramis361@gmail.com>
2024-05-03 14:37:45 +0545

    zod

--- app/auth/register/adminregister/page.js
+++ app/auth/register/adminregister/page.js
@@ -1,14 +1,47 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import axiosInstance from "@/services/axios";
 
-const page = () => {
+const RegisterPage = () => {
+  const [state, setState] = useState({
+    username: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+  });
+  const handleStateChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmitForm = async (e) => {
+    e.preventDefault();
+    const userData = {
+      username: state.username,
+      email: state.email,
+      phoneNumber: state.phoneNumber,
+      password: state.password,
+    };
+    console.log(userData);
+    try {
+      const response = await axiosInstance.post(
+        "/auth/register/admin/",
+        userData
+      );
+
+      console.log(response);
+    } catch (error) {
+      console.log("registration failed: ", error);
+    }
+  };
+
   return (
     <div>
       <Navbar />
       <div className="h-screen flex justify-center items-center bg-slate-200">
         <div className="w-4/5 h-3/4 rounded-lg md:w-2/5 bg-white">
-          <form className=" h-full py-10">
+          <form onSubmit={handleSubmitForm} className=" h-full py-10">
             <div className="text-center ">
               <h1 className="text-3xl">Admin Register</h1>
               <p className="text-primary py-4">
@@ -17,25 +50,33 @@ const page = () => {
             </div>
             <div className="gap-6 flex flex-col justify-center items-center">
               <input
+                type="text"
                 className="border rounded-sm border-primary p-2 w-2/3"
                 placeholder="Username"
+                onChange={handleStateChange}
+                name="username"
               ></input>
               <input
+                type="text"
                 className="border rounded-sm border-primary p-2 w-2/3"
                 placeholder="Email"
+                onChange={handleStateChange}
+                name="email"
               ></input>
               <input
+                type="tel"
                 className="border rounded-sm border-primary p-2 w-2/3"
                 placeholder="Phone number"
-              ></input>
-
-              <input
-                className="border rounded-sm border-primary p-2 w-2/3"
-                placeholder="Citizenship number"
+                onChange={handleStateChange}
+                name="phoneNumber"
               ></input>
               <input
+                type="password"
                 className="border rounded-sm border-primary p-2 w-2/3"
                 placeholder="Password"
+                onChange={handleStateChange}
+                autoComplete="on"
+                name="password"
               ></input>
               <button className="bg-primary text-white w-2/3 p-2 hover:scale-105 transition-all duration-300 hover:text-secondary">
                 Register now
@@ -49,4 +90,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default RegisterPage;
--- app/auth/register/userregister/page.js
+++ app/auth/register/userregister/page.js
@@ -1,14 +1,35 @@
+"use client";
 import React from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
-const page = () => {
+const registerSchema = {
+  username: z.string(),
+  email: z.string().email(),
+  phoneNumber: z.number().min(8),
+  citizenshipNumber: z.string(),
+  password: z.string().min(5),
+};
+
+const RegisterPage = () => {
+  const { register, handleSubmit, reset } = useForm({
+    resolver: zodResolver(registerSchema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <div>
       <Navbar />
       <div className="h-screen flex justify-center items-center bg-slate-200">
         <div className="w-4/5 h-3/4 rounded-lg md:w-2/5 bg-white">
-          <form className=" h-full py-10">
+          <form onSubmit={handleSubmit(onSubmit)} className=" h-full py-10">
             <div className="text-center ">
               <h1 className="text-3xl">User Register</h1>
               <p className="text-primary py-4">
@@ -17,27 +38,35 @@ const page = () => {
             </div>
             <div className="gap-6 flex flex-col justify-center items-center">
               <input
+                {...register("username")}
                 className="border rounded-sm border-primary p-2 w-2/3"
                 placeholder="Username"
               ></input>
               <input
+                {...register("email")}
                 className="border rounded-sm border-primary p-2 w-2/3"
                 placeholder="Email"
               ></input>
               <input
+                {...register("phoneNumber")}
                 className="border rounded-sm border-primary p-2 w-2/3"
                 placeholder="Phone number"
               ></input>
 
               <input
+                {...register("citizenshipNumber")}
                 className="border rounded-sm border-primary p-2 w-2/3"
                 placeholder="Citizenship number"
               ></input>
               <input
+                {...register("password")}
                 className="border rounded-sm border-primary p-2 w-2/3"
                 placeholder="Password"
               ></input>
-              <button className="bg-primary text-white w-2/3 p-2 hover:scale-105 transition-all duration-300 hover:text-secondary">
+              <button
+                type="submit"
+                className="bg-primary text-white w-2/3 p-2 hover:scale-105 transition-all duration-300 hover:text-secondary"
+              >
                 Register now
               </button>
             </div>
@@ -49,4 +78,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default RegisterPage;
